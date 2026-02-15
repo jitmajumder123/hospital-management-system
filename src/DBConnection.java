a005fa5310fefd0496218500b3d865c326bfc255
Jit Majumder <118308500+jitmajumder123@users.noreply.github.com>
2026-02-16 01:58:09 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
     private static final String URL =
             "jdbc:mysql://localhost:3306/hospital_db";
     private static final String USER = "root";
-    private static final String PASS = "Root@123";
+    private static final String PASS = "Password@123";
 
     public static Connection getConnection() {
         try {
